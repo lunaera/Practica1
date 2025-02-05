8e2120b0c19523c218c72d4b6ca1cf0908d703ed
Erahin Pacheco Luna <erahinpachecoluna@gmail.com>
2025-02-05 10:08:07 -0600

    Sintaxis e identificadores

--- src/js/Script1.js
+++ src/js/Script1.js
@@ -1,2 +1,37 @@
 console.log("Hola mundo!!")
 console.log("mensaje 2")
+let companyName = "New yotk";
+
+
+
+function multiplicacion(i, numero) {
+    return (numero * i)
+}
+
+function ejecutaTabla(multiplicacionCallBack, num) {
+    let tabla = "";
+    for (let i = 0; i <= 10; i++) {
+        tabla += `${num} * ${i} = ${multiplicacionCallBack(i, num)} ` + "\n"
+    }
+    console.log(tabla);
+}
+
+ejecutaTabla(multiplicacion, 5);
+
+// sin callback
+
+function TablaM(num) {
+    let tabla = ""
+    for (let i = 0; i <= 10; i++) {
+        tabla += `${num} * ${i} = ${num * i} ` + "\n"
+    }
+    console.log(tabla)
+}
+
+TablaM(5)
+
+function suma(i, numero) {
+    return (numero + i);
+}
+
+ejecutaTabla(suma, 5);
---
+++ src/js/script2.js
@@ -0,0 +1,99 @@
+
+// Asignación de variable
+let numero = 56;
+
+// Expresión aritmética
+let multiplicacion = (23 * 32);
+let x = 2;
+let expresion = 5 * (x) - 3 * (x) + 1;
+
+// Llamada a función
+sumar();
+//calcularPromedio();
+
+// estructura de una funcion
+function sumar(x, y) {
+
+}
+
+// Estructura de una función con valor de retorno
+function getPromedio() {
+
+    return promedio, x, y;
+}
+
+function getMessage() {
+    return "Hola";
+}
+
+// Estructura selectiva simple
+
+let condicion, condition = 5 > 2;
+if (condicion) {
+
+}
+
+// Estructura selectiva doble
+if (condicion) {
+
+} else {
+
+}
+
+// Estructura selectiva anidado
+if (condicion) {
+    if (condition) {
+
+    } else {
+
+    }
+}
+else {
+
+}
+
+// Estructura selectiva multiple
+if (condicion) {
+
+}
+else if (condicion) {
+
+}
+else if (condicion) {
+
+}
+
+// Bucles
+for (let i = 1; i <= 10; i++) {
+
+}
+
+
+// Estructura de una clase
+
+class MiClase {
+    nombre = "Erahin";
+    edad = "26";
+
+    constructor(apellidoP, apellidoM, gender) {
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.gender = "F";
+    }
+}
+
+// Manipulación de Objetos y propiedades
+
+MiClase.telefono = "9511940403";
+console.log(MiClase.telefono);
+
+// Creación de objeto forma común y sencilla
+
+
+
+// Creación de objeto usando un constructor new Object()
+
+
+//creación de objeto usando una clase ES6
+
+
